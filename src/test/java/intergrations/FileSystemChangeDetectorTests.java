13fdfef044b1e9438eec5e74fa166898108fd418
Erik Lindblom <erik@baens.net>
2014-01-20 15:34:48 -0700

    Refactored test code to reuse setup

--- src/test/java/intergrations/FileSystemChangeDetectorTests.java
+++ src/test/java/intergrations/FileSystemChangeDetectorTests.java
@@ -4,6 +4,7 @@ import change.DetectedChange;
 import change.FileSystemChangeDetector;
 import com.google.common.eventbus.Subscribe;
 import com.google.common.io.Files;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -12,39 +13,36 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileSystemChangeDetectorTests {
+    private DetectionReceiver _receiver;
+    private FileSystemChangeDetector _detector;
+    private File _tmpDir;
+
+    @Before
+    public void setup(){
+        _tmpDir = Files.createTempDir();
+        _tmpDir.deleteOnExit();
+
+        _receiver = new DetectionReceiver();
+
+        _detector = new FileSystemChangeDetector(_tmpDir.getAbsolutePath());
+
+        _detector.register(_receiver);
+    }
+
     @Test
     public void default_noChangeEventFired() throws IOException {
-        File tmpDir = Files.createTempDir();
-        tmpDir.deleteOnExit();
+        _detector.pump();
 
-        DetectionReceiver receiver = new DetectionReceiver();
-
-        FileSystemChangeDetector detector = new FileSystemChangeDetector(tmpDir.getAbsolutePath());
-
-        detector.register(receiver);
-
-        detector.pump();
-
-        assertThat(receiver.hasReceivedChanged()).isFalse();
+        assertThat(_receiver.hasReceivedChanged()).isFalse();
     }
 
     @Test
     public void fileCreated_changeEventFired() throws IOException, InterruptedException {
-        File tmpDir = Files.createTempDir();
-        tmpDir.deleteOnExit();
+        new File(_tmpDir.getAbsolutePath()+"/test").createNewFile();
 
-        DetectionReceiver receiver = new DetectionReceiver();
+        _detector.pump();
 
-        FileSystemChangeDetector detector = new FileSystemChangeDetector(tmpDir.getAbsolutePath());
-
-        detector.register(receiver);
-
-        File file = new File(tmpDir.getAbsolutePath()+"/test");
-        file.createNewFile();
-
-        detector.pump();
-
-        assertThat(receiver.hasReceivedChanged()).isTrue();
+        assertThat(_receiver.hasReceivedChanged()).isTrue();
     }
 
     class DetectionReceiver {
