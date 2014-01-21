15eee6fa5c482c09ae03d41a8932f2f600dd4d34
Erik Lindblom <erik@baens.net>
2014-01-20 21:32:44 -0700

    Hook up a simple notication of file changing

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,6 +1,30 @@
+import change.DetectedChange;
+import change.FileSystemChangeDetector;
+import com.google.common.eventbus.Subscribe;
+
+import java.nio.file.Paths;
+
 public class Console {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Runing");
-        while(true) Thread.sleep(1000);
+        System.out.println("Running");
+
+        String absolutePath = Paths.get("").toAbsolutePath().toString();
+        System.out.println(absolutePath);
+        FileSystemChangeDetector detector = new FileSystemChangeDetector(
+                absolutePath
+        );
+        detector.register(new Changer());
+
+        while(true){
+            detector.pump();
+            Thread.yield();
+        }
+    }
+
+    public static class Changer {
+        @Subscribe
+        public void change(DetectedChange e){
+            System.out.println("A file has change");
+        }
     }
 }
